3a2ef0c39a7305a89cb60505fdeabdb7f4479b58
sergiobernalparrondo <serberpar@gmail.com>
2022-02-03 15:08:16 +0100

    Fixed Permissions on node

--- src/main/java/io/github/divios/jcommands/JCommandListener.java
+++ src/main/java/io/github/divios/jcommands/JCommandListener.java
@@ -31,7 +31,7 @@ class JCommandListener implements TabCompleter, CommandExecutor {
 
         List<String> tabCompletes = new ArrayList<>();
         for (Node child : node.getChildren()) {
-            if (!meetsCommandRequirements(sender, child.getCommand())) continue;
+            if (!meetsCommandRequirements(sender, child)) continue;
 
             for (String suggestion : child.getLabel().getSuggestions())
                 if (StringUtils.startsWithIgnoreCase(suggestion, (args[args.length - 1])))
@@ -41,15 +41,15 @@ class JCommandListener implements TabCompleter, CommandExecutor {
         return tabCompletes;
     }
 
-    private boolean meetsCommandRequirements(CommandSender sender, JCommand command) {
-        return meetsPerms(sender, command) &&
-                command.getRequirements().stream()
+    private boolean meetsCommandRequirements(CommandSender sender, Node node) {
+        return meetsPerms(sender, node) &&
+                node.getCommand().getRequirements().stream()
                         .allMatch(commandSenderPredicate -> commandSenderPredicate.test(sender));
     }
 
-    private boolean meetsPerms(CommandSender sender, JCommand command) {
-        if (command.getPermission() == null || command.getPermission().getName().isEmpty()) return true;
-        return sender.hasPermission(command.getPermission());
+    private boolean meetsPerms(CommandSender sender, Node node) {
+        if (node.getPermissions() == null || node.getPermissions().isEmpty()) return true;
+        return node.getPermissions().stream().allMatch(sender::hasPermission);
     }
 
     @Override
@@ -57,7 +57,7 @@ class JCommandListener implements TabCompleter, CommandExecutor {
         Node node = commandMap.search(command.getLabel(), args);
         if (node == null) return false;
 
-        if (!meetsCommandRequirements(sender, node.getCommand())) {
+        if (!meetsCommandRequirements(sender, node)) {
             node.getCommand().getInvalidPermissionAction().accept(sender);
             return false;
         }
@@ -80,9 +80,8 @@ class JCommandListener implements TabCompleter, CommandExecutor {
 
         String[] validArgs = Arrays.copyOfRange(args, ArrayUtils.indexOf(args, command.getName()) + 1, args.length);
 
-        for (int i = 0; i < arguments.size(); i++) {
+        for (int i = 0; i < arguments.size(); i++)
             valueMap.put(arguments.get(i).getName(), Value.ofString(validArgs[i]));
-        }
 
         return ValueMap.of(valueMap);
     }
